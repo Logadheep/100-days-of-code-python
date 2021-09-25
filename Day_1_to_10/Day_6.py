00c34ca1f650e1dc899173c3c438d1b9eccc7fc6
mr.anon.programmer <76146062+Logadheep@users.noreply.github.com>
2021-09-25 15:22:26 +0530

    Update Day_6.py

--- Day_1_to_10/Day_6.py
+++ Day_1_to_10/Day_6.py
@@ -1,9 +1,12 @@
 x, y = map(int, input().split())
 
+# the output .|.|. till welcome line
 for i in range(1, x, 2):
     print(((y - 3 * i) // 2) * '-' + i * '.|.' + ((y - 3 * i) // 2) * '-')
 
+# Welcome line
 print(((y - 7) // 2) * '-' + 'WELCOME' + ((y - 7) // 2) * '-')
 
+# After welcome
 for i in range(x - 2, -1, -2):
     print(((y - 3 * i) // 2) * '-' + i * '.|.' + ((y - 3 * i) // 2) * '-')
